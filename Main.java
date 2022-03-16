791fb95caa15b03ff61e45b1044e91fae420c39c
AkramSultani <akram.nadri@gmail.com>
2022-03-16 19:28:19 -0400

    Min/Max

--- Main.java
+++ Main.java
@@ -1,2 +1,50 @@
 // Udemy Java course 2022
 // We Mar 16th 2022
+
+
+// public class Main{
+
+//     public static void main(String [] args){
+
+//     }
+// }
+
+
+// public class Main{
+
+//     public static void main(String [] args){
+
+
+//     // Decleration statement used to define variable by indicating data type, the name and optionally set the variable value.    
+//     // int myFirstNumber = (5 + 10) + (2 + 5);
+//     // int mySecondNumber = 12;
+//     // int myThirdNumber = myFirstNumber * 2;
+
+//     // int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+//     // System.out.println(myTotal);
+
+//     // int myLastOne = 1000 - myTotal;
+//     // System.out.println(myLastOne);
+
+
+//     }
+// }  
+
+/////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
+
+
+
+
+public class Main{
+
+    public static void main(String [] args){
+
+    int myValue = 10000;
+
+    int myMinValue = Integer.MIN_VALUE;
+
+    }
+}
